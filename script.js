83667b118e71a572ae938adfa85ec955ac7c088c
pdstavreva-web <p.d.stavreva@gmail.com>
2026-06-15 03:07:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+alert("SCRIPT LOADED");
+
 function nextPage(page){
 
 document
@@ -7,46 +9,5 @@ document
 document
 .getElementById("page" + page)
 .classList.add("active");
-
-if(page === 12){
-
-confetti({
-particleCount:150,
-spread:120,
-origin:{y:0.6}
-});
-
-const music =
-document.getElementById("birthdayMusic");
-
-if(music){
-music.play();
-}
-
-}
-
-}
-
-const noBtn =
-document.getElementById("noBtn");
-
-if(noBtn){
-
-noBtn.addEventListener(
-"mouseover",
-()=>{
-
-const x =
-Math.random()*400;
-
-const y =
-Math.random()*300;
-
-noBtn.style.position="absolute";
-noBtn.style.left=x+"px";
-noBtn.style.top=y+"px";
-
-}
-);
 
 }
